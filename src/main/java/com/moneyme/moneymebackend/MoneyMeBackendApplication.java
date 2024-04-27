9ad55ce5e3fc7f10896c8e3d3fd790767ffd989f
takafumi11 <noirit7@gmail.com>
2024-04-28 02:19:22 +0900

    initial commit

---
+++ src/main/java/com/moneyme/moneymebackend/MoneyMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moneyme.moneymebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneyme/moneymebackend/MoneyMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneyme.moneymebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
