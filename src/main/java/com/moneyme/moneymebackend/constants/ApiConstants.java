9b6c247fff60e7aa964067b2edea82eabeeaac64
takafumi11 <noirit7@gmail.com>
2024-05-02 02:04:54 +0900

    final test

--- src/main/java/com/moneyme/moneymebackend/constants/ApiConstants.java
+++ src/main/java/com/moneyme/moneymebackend/constants/ApiConstants.java
@@ -5,7 +5,7 @@ public class ApiConstants {
     public static final String HEADER_AUTHORIZATION = "Authorization";
 
     // API paths
-    public static final String PATH_USERS = "/usersss";
+    public static final String PATH_USERS = "/users";
     public static final String PATH_LOANS = "/loans";
     public static final String PATH_REPAYMENTS = "/repayments";
     public static final String PATH_GROUPS = "/groups";
