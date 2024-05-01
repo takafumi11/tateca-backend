39bc49a160a00f883d4024ee417333a3420db762
takafumi11 <noirit7@gmail.com>
2024-05-01 23:58:45 +0900

    added get-transactions

---
+++ src/main/java/com/moneyme/moneymebackend/service/TransactionService.java
@@ -0,0 +1,38 @@
+package com.moneyme.moneymebackend.service;
+
+import com.moneyme.moneymebackend.dto.response.GetTransactionsResponse;
+import com.moneyme.moneymebackend.entity.LoanEntity;
+import com.moneyme.moneymebackend.entity.RepaymentEntity;
+import com.moneyme.moneymebackend.repository.LoanRepository;
+import com.moneyme.moneymebackend.repository.RepaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+    private final LoanRepository loanRepository;
+    private final RepaymentRepository repaymentRepository;
+
+    public List<GetTransactionsResponse> getTransactions(int count, String groupId) {
+        UUID groupUuid = UUID.fromString(groupId);
+
+        List<LoanEntity> loans = loanRepository.getLoansByGroup(groupUuid, PageRequest.of(0, count));
+        List<RepaymentEntity> repayments = repaymentRepository.getRepaymentsByGroup(groupUuid, PageRequest.of(0, count));
+
+        List<GetTransactionsResponse> transactionsResponses = loans.stream().map(GetTransactionsResponse::from).toList();
+        List<GetTransactionsResponse> transactionsResponses2 = repayments.stream().map(GetTransactionsResponse::from).toList();
+
+        List<GetTransactionsResponse> combinedResponses = Stream.concat(transactionsResponses.stream(), transactionsResponses2.stream()).toList();
+
+        return combinedResponses.stream().sorted((t1, t2) -> t2.getCreatedAt().compareTo(t1.getCreatedAt()))
+                .limit(count)
+                .toList();
+    }
+
+}
