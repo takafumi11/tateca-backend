be5e663e225a1efd2e2a631ff9581f4bdef4d901
takafumi11 <noirit7@gmail.com>
2024-05-21 23:36:38 +0900

    init firebase

---
+++ src/main/java/com/moneyme/moneymebackend/service/FirebaseAuthService.java
@@ -0,0 +1,14 @@
+package com.moneyme.moneymebackend.service;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseAuthService {
+
+    public FirebaseToken verifyIdToken(String idToken) throws FirebaseAuthException {
+        return FirebaseAuth.getInstance().verifyIdToken(idToken);
+    }
+}
