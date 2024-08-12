72fade35b9ebdc97fe958f2229d87529861240f1
takafumi11 <noirit7@gmail.com>
2024-08-12 17:09:54 +0900

    created authUserEntity and updated user enitty

--- src/main/java/com/moneyme/moneymebackend/entity/AuthUserEntity.java
+++ src/main/java/com/moneyme/moneymebackend/entity/AuthUserEntity.java
@@ -2,10 +2,7 @@ package com.moneyme.moneymebackend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -22,18 +19,20 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "users")
-public class UserEntity {
+@Table(name = "`auth_users`")
+public class AuthUserEntity {
     @Id
     @Column(columnDefinition = "BINARY(16)")
     private UUID uuid;
 
-    @Column(name = "name", nullable = false, length = 50)
+    @Column(name = "name", length = 50)
     private String name;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "auth_user_uuid", nullable = false)
-    private UserEntity authUser;
+    @Column(name = "email", unique = true, length = 255)
+    private String email;
+
+    @Column(name = "uid", unique = true, nullable = false)
+    private String uid;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private ZonedDateTime createdAt;
@@ -52,5 +51,4 @@ public class UserEntity {
     protected void onUpdate() {
         updatedAt = ZonedDateTime.now();
     }
-
 }
