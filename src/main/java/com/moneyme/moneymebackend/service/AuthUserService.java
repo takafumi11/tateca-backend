ce0e72a39f5c36daeaa6f6c93161baae8ebf114f
takafumi11 <noirit7@gmail.com>
2024-08-15 16:01:16 +0900

    added delete auth user

--- src/main/java/com/moneyme/moneymebackend/service/AuthUserService.java
+++ src/main/java/com/moneyme/moneymebackend/service/AuthUserService.java
@@ -1,19 +1,25 @@
 package com.moneyme.moneymebackend.service;
 
 import com.moneyme.moneymebackend.accessor.AuthUserAccessor;
+import com.moneyme.moneymebackend.accessor.UserAccessor;
 import com.moneyme.moneymebackend.dto.request.AuthUserRequestDTO;
+import com.moneyme.moneymebackend.dto.request.UserDeleteRequestDTO;
 import com.moneyme.moneymebackend.dto.response.AuthUserResponseDTO;
+import com.moneyme.moneymebackend.dto.response.UserResponseDTO;
 import com.moneyme.moneymebackend.entity.AuthUserEntity;
+import com.moneyme.moneymebackend.entity.UserEntity;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AuthUserService {
     private final AuthUserAccessor accessor;
+    private final UserAccessor userAccessor;
 
     public AuthUserResponseDTO getAuthUserInfo(String uid) {
         AuthUserEntity authUser = accessor.findByUid(uid);
@@ -22,6 +28,8 @@ public class AuthUserService {
 
     @Transactional
     public AuthUserResponseDTO createAuthUser(String uid, AuthUserRequestDTO request) {
+        accessor.validateEmail(request.getEmail());
+
         AuthUserEntity authUser = AuthUserEntity.builder()
                 .uid(uid)
                 .name(request.getName())
@@ -31,5 +39,16 @@ public class AuthUserService {
         AuthUserEntity createdAuthUser = accessor.save(authUser);
 
         return AuthUserResponseDTO.from(createdAuthUser);
+    }
+
+    @Transactional
+    public void deleteAuthUser(String uid) {
+        List<UserEntity> userEntityList = userAccessor.findByAuthUserUid(uid);
+        userEntityList.forEach(user -> {
+            user.setAuthUser(null);
+        });
+
+        userAccessor.saveAll(userEntityList);
+        accessor.deleteById(uid);
     }
 }
