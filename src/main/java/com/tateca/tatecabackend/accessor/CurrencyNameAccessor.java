bfe49015459cc26955ac93f33735366e88809948
takafumi11 <noirit7@gmail.com>
2025-06-08 13:29:32 +0900

    added controller, repo for lambda

--- src/main/java/com/tateca/tatecabackend/accessor/CurrencyNameAccessor.java
+++ src/main/java/com/tateca/tatecabackend/accessor/CurrencyNameAccessor.java
@@ -4,6 +4,7 @@ import com.tateca.tatecabackend.entity.CurrencyNameEntity;
 import com.tateca.tatecabackend.repository.CurrencyNameRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
@@ -44,6 +45,18 @@ public class CurrencyNameAccessor {
     public List<CurrencyNameEntity> findAll() {
         try {
             return repository.findAll();
+        } catch (DataAccessException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Database error", e);
+        }
+    }
+    
+    public CurrencyNameEntity findFirstCurrencyCode() {
+        try {
+            List<CurrencyNameEntity> currencies = repository.findFirstCurrencyCode(PageRequest.of(0, 1));
+            if (currencies.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No currency codes found");
+            }
+            return currencies.get(0);
         } catch (DataAccessException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Database error", e);
         }
