b67e3d9ae4dad005b62544e98dc57c89de4367d9
takafumi11 <64940149+takafumi11@users.noreply.github.com>
2025-08-09 22:37:37 +0900

    Hotfix/revert web client (#132)
    * Reverted web config
    
    * Replated restTemplate with webClient

--- src/main/java/com/tateca/tatecabackend/config/AppConfig.java
+++ src/main/java/com/tateca/tatecabackend/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableScheduling
@@ -13,5 +14,10 @@ public class AppConfig {
         return WebClient.builder()
             .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024)) // 1MB
             .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
