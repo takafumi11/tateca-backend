1d11e1f45e665b70a44da27447a0655a26d9d0ec
takafumi11 <noirit7@gmail.com>
2025-08-11 13:26:50 +0900

    revert: exchange rate

--- src/main/java/com/tateca/tatecabackend/dto/response/ExchangeRateResponseDTO.java
+++ src/main/java/com/tateca/tatecabackend/dto/response/ExchangeRateResponseDTO.java
@@ -1,7 +1,9 @@
 package com.tateca.tatecabackend.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tateca.tatecabackend.entity.CurrencyNameEntity;
 import com.tateca.tatecabackend.entity.ExchangeRateEntity;
+import com.tateca.tatecabackend.model.SymbolPosition;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,13 +12,28 @@ import java.math.BigDecimal;
 @Data
 @Builder
 public class ExchangeRateResponseDTO {
-    @JsonProperty("currency_name") String currencyName;
-    @JsonProperty("rate") BigDecimal rate;
+    @JsonProperty("currency_code") String currencyCode;
+    @JsonProperty("jp_currency_name") String jpCurrencyName;
+    @JsonProperty("eng_currency_name") String engCurrencyName;
+    @JsonProperty("jp_country_name") String jpCountryName;
+    @JsonProperty("eng_country_name") String engCountryName;
+    @JsonProperty("currency_symbol") String currencySymbol;
+    @JsonProperty("symbol_position")
+    SymbolPosition symbolPosition;
+    @JsonProperty("exchange_rate") String exchangeRate;
 
-    public static ExchangeRateResponseDTO from(ExchangeRateEntity entity) {
+    public static ExchangeRateResponseDTO from(ExchangeRateEntity exchangeRateEntity) {
+        CurrencyNameEntity currencyNameEntity = exchangeRateEntity.getCurrencyName();
+
         return ExchangeRateResponseDTO.builder()
-                .currencyName(entity.getCurrencyName().getEngCurrencyName())
-                .rate(entity.getExchangeRate())
+                .currencyCode(currencyNameEntity.getCurrencyCode())
+                .jpCurrencyName(currencyNameEntity.getJpCurrencyName())
+                .engCurrencyName(currencyNameEntity.getEngCurrencyName())
+                .jpCountryName(currencyNameEntity.getJpCountryName())
+                .engCountryName(currencyNameEntity.getEngCountryName())
+                .currencySymbol(currencyNameEntity.getCurrencySymbol())
+                .symbolPosition(currencyNameEntity.getSymbolPosition())
+                .exchangeRate(exchangeRateEntity.getExchangeRate().toString())
                 .build();
     }
 }
