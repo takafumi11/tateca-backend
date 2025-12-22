c009187ee48b1c1a651ccc10e2fe05ad35ffa599
takafumi11 <noirit7@gmail.com>
2025-12-22 22:00:30 +0900

    feat: Add SpringDoc annotations to GroupController

--- src/main/java/com/tateca/tatecabackend/dto/request/CreateGroupRequestDTO.java
+++ src/main/java/com/tateca/tatecabackend/dto/request/CreateGroupRequestDTO.java
@@ -1,13 +1,23 @@
 package com.tateca.tatecabackend.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Schema(description = "Request to create a new group")
 public class CreateGroupRequestDTO {
-    @JsonProperty("group_name") String groupName;
-    @JsonProperty("host_name") String hostName;
-    @JsonProperty("participants_name") List<String> participantsName;
+    @JsonProperty("group_name")
+    @Schema(description = "Name of the group", example = "Team Outing 2024")
+    String groupName;
+
+    @JsonProperty("host_name")
+    @Schema(description = "Name of the group host", example = "John Doe")
+    String hostName;
+
+    @JsonProperty("participants_name")
+    @Schema(description = "List of participant names", example = "[\"Alice\", \"Bob\", \"Charlie\"]")
+    List<String> participantsName;
 }
