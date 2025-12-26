25537f8316ed9e9d0b2074a72199bfbb771f8b14
takafumi11 <noirit7@gmail.com>
2025-12-26 19:05:16 +0900

    refactor: Remove unused BearerTokenInterceptor and update LoggingInterceptor to use SecurityContext

--- src/main/java/com/tateca/tatecabackend/constants/AttributeConstants.java
+++ src/main/java/com/tateca/tatecabackend/constants/AttributeConstants.java
@@ -2,7 +2,6 @@ package com.tateca.tatecabackend.constants;
 
 public class AttributeConstants {
 
-    public static final String UID_ATTRIBUTE = "uid";
     public static final String REQUEST_TIME_ATTRIBUTE = "requestTime";
     public static final String REQUEST_ID_ATTRIBUTE = "requestId";
 }
