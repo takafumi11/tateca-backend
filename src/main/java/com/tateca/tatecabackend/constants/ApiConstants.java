850702dc613635952be8e2974b499a1b8d698d5f
takafumi11 <64940149+takafumi11@users.noreply.github.com>
2025-12-30 02:50:19 +0900

    Revert "Feature/modify repayment calculation (#227)" (#228)
    This reverts commit 7d86e796b355ff8fa5eaba61f934efa4f177572f.

--- src/main/java/com/tateca/tatecabackend/constants/ApiConstants.java
+++ src/main/java/com/tateca/tatecabackend/constants/ApiConstants.java
@@ -11,6 +11,7 @@ public class ApiConstants {
     public static final String PATH_GROUPS = "/groups";
     public static final String PATH_TRANSACTIONS = "/transactions";
     public static final String PATH_HISTORY = "/history";
+    public static final String PATH_SETTLEMENT = "/settlement";
     public static final String PATH_EXCHANGE_RATE = "/exchange-rate";
     public static final String PATH_DEV = "/dev";
 }
--- src/main/java/com/tateca/tatecabackend/dto/response/TransactionsSettlementResponseDTO.java
+++ src/main/java/com/tateca/tatecabackend/dto/response/TransactionsSettlementResponseDTO.java
@@ -2,28 +2,19 @@ package com.tateca.tatecabackend.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 @Schema(description = "Settlement information for a group")
-public record TransactionsSettlementResponseDTO(
+public class TransactionsSettlementResponseDTO {
    @JsonProperty("transactions_settlement")
    @Schema(description = "List of settlements needed to balance accounts")
-   List<TransactionSettlement> transactionsSettlement
-) {
-    @Schema(description = "Settlement transaction between two users")
-    public record TransactionSettlement(
-        @JsonProperty("from")
-        @Schema(description = "User who owes money")
-        UserResponseDTO from,
+   List<TransactionSettlementResponseDTO> transactionsSettlement;
 
-        @JsonProperty("to")
-        @Schema(description = "User who is owed money")
-        UserResponseDTO to,
-
-        @JsonProperty("amount")
-        @Schema(description = "Amount to be settled in cents", example = "3500")
-        long amount
-    ) {}
+   @JsonProperty("currency_code")
+   @Schema(description = "Currency code for settlements", example = "JPY")
+   String currencyCode;
 }
 
