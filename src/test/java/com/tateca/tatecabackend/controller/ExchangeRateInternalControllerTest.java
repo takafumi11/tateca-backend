4be9622d3d1000d98f40f803f3d6ea3ec8f2da5d
takafumi11 <noirit7@gmail.com>
2026-01-03 22:40:53 +0900

    Updated test

--- src/test/java/com/tateca/tatecabackend/controller/ExchangeRateInternalControllerTest.java
+++ src/test/java/com/tateca/tatecabackend/controller/ExchangeRateInternalControllerTest.java
@@ -24,8 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ExchangeRateLambdaController.class)
 @Import({GlobalExceptionHandler.class, TestSecurityConfig.class})
 @ActiveProfiles("test")
-@DisplayName("ExchangeRateLambdaController Web Tests")
-class ExchangeRateResponseLambdaControllerTest {
+@DisplayName("ExchangeRate Internal API Controller Tests")
+class ExchangeRateInternalControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
