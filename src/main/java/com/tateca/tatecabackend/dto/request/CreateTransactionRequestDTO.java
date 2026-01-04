e1ff627fc51a8a1e727cbf1e8f3239c1c41c1496
takafumi11 <noirit7@gmail.com>
2026-01-04 21:02:25 +0900

    removed validation from date in cretae transaction

--- src/main/java/com/tateca/tatecabackend/dto/request/CreateTransactionRequestDTO.java
+++ src/main/java/com/tateca/tatecabackend/dto/request/CreateTransactionRequestDTO.java
@@ -43,7 +43,7 @@ public record CreateTransactionRequestDTO(
         String currencyCode,
 
         @NotBlank(message = "Date is required and cannot be blank")
-        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Date must be in yyyy-MM-dd format")
+//        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Date must be in yyyy-MM-dd format")
         @JsonProperty("date_str")
         @Schema(description = "Transaction date in yyyy-MM-dd format", example = "2024-01-15", requiredMode = Schema.RequiredMode.REQUIRED)
         String dateStr,
